e8e5db1d50a0833603f830cd5fb2da3cdd3ddbcc
brian <brian@eolas>
2020-05-08 14:55:12 -0400

    [test] growing_block_allocator tests

--- test/test_growing_block_allocator.cpp
+++ test/test_growing_block_allocator.cpp
@@ -1,3 +1,4 @@
+#include "allocator/heap_allocator.hpp"
 #include "allocator/memory_arena.hpp"
 #include <catch2/catch.hpp>
 #include <fmt/format.h>
@@ -5,10 +6,96 @@
 #include <cstdlib> // std::malloc, std::free
 
 
+struct malloc_allocator
+{
+    static std::size_t const alignment = 8;
+    static std::size_t const node_size = 64;
+
+    static void*
+    allocate_node(std::size_t size, std::size_t alignment)
+    {
+        fmt::print("malloc_allocator::allocate_node({}, {})\n", size, alignment);
+        return std::malloc(size);
+    }
+
+    static void*
+    allocate_array(std::size_t count, std::size_t size, std::size_t alignment)
+    {
+        fmt::print("malloc_allocator::allocate_array({}, {}, {})\n", count, size, alignment);
+        return std::malloc(size * count);
+    }
+
+    static void
+    deallocate_node(void* ptr, std::size_t size, std::size_t alignment)
+    {
+        fmt::print("malloc_allocator::deallocate_node({}, {}, {})\n", ptr, size, alignment);
+        std::free(ptr);
+    }
+
+    static void
+    deallocate_array(void* ptr, std::size_t count, std::size_t size, std::size_t alignment)
+    {
+        fmt::print("malloc_allocator::deallocate_array({}, {}, {}, {})\n", ptr, count, size,
+                alignment);
+        std::free(ptr);
+    }
+
+    static std::size_t
+    max_alignment()
+    {
+        fmt::print("malloc_allocator::max_alignment()\n");
+        return alignment;
+    }
+
+    static std::size_t
+    max_node_size()
+    {
+        fmt::print("malloc_allocator::max_node_size()\n");
+        return node_size;
+    }
+
+    static std::size_t
+    max_array_size()
+    {
+        fmt::print("malloc_allocator::max_array_size()\n");
+        return node_size * 10;
+    }
+};
+
 TEST_CASE("growing_block_allocator", "[growing_block_allocator]")
 {
     SECTION("ctor")
     {
-        growing_block_allocator<> gba(4096);
+        growing_block_allocator<> gba1(4096);
+        REQUIRE(gba1.growth_factor() == Approx(2.0));
+        REQUIRE(gba1.next_block_size() == 4096);
+        lowlevel_allocator<detail::heap_allocator_impl>& ref = gba1.get_allocator();
+        void* ptr = ref.allocate_node(128, 8);
+        REQUIRE(ptr != nullptr);
+        ref.deallocate_node(ptr, 128, 8);
+
+        growing_block_allocator<heap_allocator, 3, 2> gba2(1024);
+        REQUIRE(gba2.growth_factor() == Approx(1.5));
+        REQUIRE(gba2.next_block_size() == 1024);
+    }
+
+    SECTION("alloc-dealloc")
+    {
+        growing_block_allocator<malloc_allocator> gba(1024);
+        REQUIRE(gba.growth_factor() == Approx(2.0));
+
+        REQUIRE(gba.next_block_size() == 1024);
+        memory_block mb1 = gba.allocate_block();
+        REQUIRE(mb1.memory != nullptr);
+        REQUIRE(mb1.size == 1024);
+
+        REQUIRE(gba.next_block_size() == 2048);
+        memory_block mb2 = gba.allocate_block();
+        REQUIRE(mb2.memory != nullptr);
+        REQUIRE(mb2.memory != mb1.memory);
+        REQUIRE(mb2.size == 2048);
+
+        gba.deallocate_block(mb1);
+        gba.deallocate_block(mb2);
     }
 }
