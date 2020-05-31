4e8ff378c4edc53e5e3e000dd3f54a4f0085055c
brian <brian@eolas>
2020-05-31 13:13:58 -0400

    [growing_block_allocator] separate out impl

--- src/allocator/growing_block_allocator.hpp
+++ src/allocator/growing_block_allocator.hpp
@@ -14,47 +14,63 @@ class growing_block_allocator : private LLAllocator
 
 public:
     constexpr explicit growing_block_allocator(
-            std::size_t block_size, LLAllocator alloc = LLAllocator()) noexcept
-            : LLAllocator(std::move(alloc))
-            , block_size_(block_size)
-    {
-        // empty
-    }
-
-    memory_block
-    allocate_block()
-    {
-        void* memory = LLAllocator::allocate_node(block_size_, 8);
-        memory_block block(memory, block_size_);
-        block_size_ = std::size_t(block_size_ * growth_factor());
-        return block;
-    }
-
-    void
-    deallocate_block(memory_block block) noexcept
-    {
-        LLAllocator::deallocate_node(block.memory, block.size, 8);
-    }
-
-    std::size_t
-    next_block_size() const noexcept
-    {
-        return block_size_;
-    }
-
-    LLAllocator&
-    get_allocator() noexcept
-    {
-        return *this;
-    }
-
-    static float
-    growth_factor() noexcept
-    {
-        static constexpr auto factor = static_cast<float>(Num) / Den;
-        return factor;
-    }
+            std::size_t block_size, LLAllocator alloc = LLAllocator()) noexcept;
+    memory_block allocate_block();
+    void deallocate_block(memory_block block) noexcept;
+    std::size_t next_block_size() const noexcept;
+    LLAllocator& get_allocator() noexcept;
+    static float growth_factor() noexcept;
 
 private:
     std::size_t block_size_ = 0;
 };
+
+/**********************************************************************/
+
+template <typename LLAllocator, std::uint16_t Num, std::uint16_t Den>
+constexpr growing_block_allocator<LLAllocator, Num, Den>::growing_block_allocator(
+        std::size_t block_size, LLAllocator alloc) noexcept
+        : LLAllocator(std::move(alloc))
+        , block_size_(block_size)
+{
+    // empty
+}
+
+template <typename LLAllocator, std::uint16_t Num, std::uint16_t Den>
+memory_block
+growing_block_allocator<LLAllocator, Num, Den>::allocate_block()
+{
+    void* memory = LLAllocator::allocate_node(block_size_, 8);
+    memory_block block(memory, block_size_);
+    block_size_ = std::size_t(block_size_ * growth_factor());
+    return block;
+}
+
+template <typename LLAllocator, std::uint16_t Num, std::uint16_t Den>
+void
+growing_block_allocator<LLAllocator, Num, Den>::deallocate_block(memory_block block) noexcept
+{
+    LLAllocator::deallocate_node(block.memory, block.size, 8);
+}
+
+template <typename LLAllocator, std::uint16_t Num, std::uint16_t Den>
+std::size_t
+growing_block_allocator<LLAllocator, Num, Den>::next_block_size() const noexcept
+{
+    return block_size_;
+}
+
+template <typename LLAllocator, std::uint16_t Num, std::uint16_t Den>
+LLAllocator&
+growing_block_allocator<LLAllocator, Num, Den>::get_allocator() noexcept
+{
+    return *this;
+}
+
+template <typename LLAllocator, std::uint16_t Num, std::uint16_t Den>
+float
+growing_block_allocator<LLAllocator, Num, Den>::growth_factor() noexcept
+{
+    static constexpr auto factor = static_cast<float>(Num) / Den;
+    return factor;
+}
--- src/allocator/memory_arena.hpp
+++ src/allocator/memory_arena.hpp
@@ -1,59 +1,14 @@
 #pragma once
 
 #include "memory_block.hpp"
+#include "memory_block_stack.hpp"
 #include "common/assert.hpp"
 #include <cstdint>
 #include <utility>
 
 
-/// stores memory block in an intrusive linked list and allows LIFO access
-class memory_block_stack
-{
-public:
-    memory_block_stack() noexcept;
-    ~memory_block_stack() noexcept;
-    memory_block_stack(memory_block_stack&& other) noexcept;
-    memory_block_stack& operator=(memory_block_stack&& other) noexcept;
-    friend void swap(memory_block_stack& a, memory_block_stack& b) noexcept;
-
-    // the raw allocated block returned from an allocator
-    using allocated_mb = memory_block;
-
-    // the inserted block slightly smaller to allow for the fixup value
-    using inserted_mb = memory_block;
-
-    // how much an inserted block is smaller
-    static const std::size_t implementation_offset;
-
-    void push(allocated_mb block) noexcept;
-    allocated_mb pop() noexcept;
-    inserted_mb top() const noexcept;
-    bool empty() const noexcept;
-    bool owns(const void* ptr) const noexcept;
-
-    // O(n) size
-    std::size_t size() const noexcept;
-
-private:
-    struct node
-    {
-        node* prev = nullptr;
-        std::size_t usable_size = 0;
-
-        node(node* p, std::size_t size) noexcept;
-
-        static const std::size_t div_alignment;
-        static const std::size_t mod_offset;
-        static const std::size_t offset;
-    };
-
-    node* head_ = nullptr;
-};
-
-/**********************************************************************/
-
 template <typename BlockAllocator>
-class memory_arena : BlockAllocator
+class memory_arena : private BlockAllocator
 {
 public:
     using allocator_type = BlockAllocator;
