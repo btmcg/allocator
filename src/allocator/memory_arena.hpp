ef0d9b089f5f69d7e55e23de752bf2993611e999
brian <brian@eolas>
2020-05-31 15:06:54 -0400

    [memory_arena] separate impl and constexpr

--- src/allocator/memory_arena.hpp
+++ src/allocator/memory_arena.hpp
@@ -10,95 +10,113 @@
 template <typename BlockAllocator>
 class memory_arena : private BlockAllocator
 {
-public:
-    using allocator_type = BlockAllocator;
-
-    template <typename... Args>
-    explicit memory_arena(std::size_t block_size, Args&&... args)
-            : allocator_type(block_size, std::forward<Args>(args)...)
-    {
-        // empty
-    }
-
-    /// Deallocates all memory blocks that where requested back
-    /// to the BlockAllocator.
-    ~memory_arena() noexcept
-    {
-        while (!used_.empty())
-            allocator_type::deallocate_block(used_.pop());
-    }
-
-    memory_arena(memory_arena&& other) noexcept
-            : allocator_type(std::move(other))
-            , used_(std::move(other.used_))
-    {}
-
-    memory_arena&
-    operator=(memory_arena&& other) noexcept
-    {
-        memory_arena tmp(std::move(other));
-        swap(*this, tmp);
-        return *this;
-    }
-
-    friend void
-    swap(memory_arena& a, memory_arena& b) noexcept
-    {
-        std::swap(static_cast<allocator_type&>(a), static_cast<allocator_type&>(b));
-        std::swap(a.used_, b.used_);
-    }
-
-    memory_block
-    allocate_block()
-    {
-        used_.push(allocator_type::allocate_block());
-
-        auto block = used_.top();
-        return block;
-    }
-
-    memory_block
-    current_block() const noexcept
-    {
-        return used_.top();
-    }
-
-    void
-    deallocate_block() noexcept
-    {
-        get_allocator().deallocate_block(used_.pop());
-    }
-
-    bool
-    owns(const void* ptr) const noexcept
-    {
-        return used_.owns(ptr);
-    }
-
-    std::size_t
-    capacity() const noexcept
-    {
-        return size();
-    }
-
-    std::size_t
-    size() const noexcept
-    {
-        return used_.size();
-    }
-
-    std::size_t
-    next_block_size() const noexcept
-    {
-        return allocator_type::next_block_size() - memory_block_stack::implementation_offset;
-    }
-
-    allocator_type&
-    get_allocator() noexcept
-    {
-        return *this;
-    }
-
 private:
     memory_block_stack used_;
+
+public:
+    constexpr explicit memory_arena(std::size_t block_size);
+
+    /// Deallocates all memory blocks that where requested back to the
+    /// BlockAllocator.
+    constexpr ~memory_arena() noexcept;
+
+    memory_arena(memory_arena&& other) noexcept;
+    memory_arena& operator=(memory_arena&& other) noexcept;
+
+    memory_block allocate_block();
+    constexpr memory_block current_block() const noexcept;
+    void deallocate_block() noexcept;
+    constexpr bool owns(void const* ptr) const noexcept;
+    constexpr std::size_t size() const noexcept;
+    constexpr std::size_t next_block_size() const noexcept;
+    BlockAllocator& get_allocator() noexcept;
+
+    friend void swap(memory_arena& a, memory_arena& b) noexcept
+    {
+        std::swap(static_cast<BlockAllocator&>(a), static_cast<BlockAllocator&>(b));
+        std::swap(a.used_, b.used_);
+    }
 };
+
+/**********************************************************************/
+
+template <typename BlockAllocator>
+constexpr memory_arena<BlockAllocator>::memory_arena(std::size_t block_size)
+        : BlockAllocator(block_size)
+{
+    // empty
+}
+
+template <typename BlockAllocator>
+constexpr memory_arena<BlockAllocator>::~memory_arena() noexcept
+{
+    while (!used_.empty())
+        BlockAllocator::deallocate_block(used_.pop());
+}
+
+template <typename BlockAllocator>
+memory_arena<BlockAllocator>::memory_arena(memory_arena&& other) noexcept
+        : BlockAllocator(std::move(other))
+        , used_(std::move(other.used_))
+{
+    // empty
+}
+
+template <typename BlockAllocator>
+memory_arena<BlockAllocator>&
+memory_arena<BlockAllocator>::operator=(memory_arena&& other) noexcept
+{
+    memory_arena tmp(std::move(other));
+    swap(*this, tmp);
+    return *this;
+}
+
+template <typename BlockAllocator>
+memory_block
+memory_arena<BlockAllocator>::allocate_block()
+{
+    used_.push(BlockAllocator::allocate_block());
+    return used_.top();
+}
+
+template <typename BlockAllocator>
+constexpr memory_block
+memory_arena<BlockAllocator>::current_block() const noexcept
+{
+    return used_.top();
+}
+
+template <typename BlockAllocator>
+void
+memory_arena<BlockAllocator>::deallocate_block() noexcept
+{
+    BlockAllocator::deallocate_block(used_.pop());
+}
+
+template <typename BlockAllocator>
+constexpr bool
+memory_arena<BlockAllocator>::owns(const void* ptr) const noexcept
+{
+    return used_.owns(ptr);
+}
+
+template <typename BlockAllocator>
+constexpr std::size_t
+memory_arena<BlockAllocator>::size() const noexcept
+{
+    return used_.size();
+}
+
+template <typename BlockAllocator>
+constexpr std::size_t
+memory_arena<BlockAllocator>::next_block_size() const noexcept
+{
+    return BlockAllocator::next_block_size() - memory_block_stack::implementation_offset;
+}
+
+template <typename BlockAllocator>
+BlockAllocator&
+memory_arena<BlockAllocator>::get_allocator() noexcept
+{
+    return *this;
+}
--- test/test_memory_arena.cpp
+++ test/test_memory_arena.cpp
@@ -11,14 +11,12 @@ TEST_CASE("memory_arena", "[memory_arena]")
     SECTION("ctor")
     {
         memory_arena<growing_block_allocator<>> ma(1024);
-        REQUIRE(ma.capacity() == 0);
         REQUIRE(ma.size() == 0);
     }
 
     SECTION("move ctor")
     {
         memory_arena<growing_block_allocator<>> ma1(1024);
-        REQUIRE(ma1.capacity() == 0);
         REQUIRE(ma1.size() == 0);
 
         memory_block mb = ma1.allocate_block();
@@ -26,13 +24,11 @@ TEST_CASE("memory_arena", "[memory_arena]")
         REQUIRE(mb.size == 1024 - 16);
         REQUIRE(ma1.current_block().memory == mb.memory);
         REQUIRE(ma1.current_block().size == mb.size);
-        REQUIRE(ma1.capacity() == 1);
         REQUIRE(ma1.size() == 1);
 
         memory_arena<growing_block_allocator<>> ma2(std::move(ma1));
         REQUIRE(ma2.current_block().memory == mb.memory);
         REQUIRE(ma2.current_block().size == mb.size);
-        REQUIRE(ma2.capacity() == 1);
         REQUIRE(ma2.size() == 1);
 
         ma2.deallocate_block();
@@ -41,7 +37,6 @@ TEST_CASE("memory_arena", "[memory_arena]")
     SECTION("move assignment")
     {
         memory_arena<growing_block_allocator<>> ma1(1024);
-        REQUIRE(ma1.capacity() == 0);
         REQUIRE(ma1.size() == 0);
 
         memory_block mb = ma1.allocate_block();
@@ -49,14 +44,12 @@ TEST_CASE("memory_arena", "[memory_arena]")
         REQUIRE(mb.size == 1024 - 16);
         REQUIRE(ma1.current_block().memory == mb.memory);
         REQUIRE(ma1.current_block().size == mb.size);
-        REQUIRE(ma1.capacity() == 1);
         REQUIRE(ma1.size() == 1);
 
         memory_arena<growing_block_allocator<>> ma2(0);
         ma2 = std::move(ma1);
         REQUIRE(ma2.current_block().memory == mb.memory);
         REQUIRE(ma2.current_block().size == mb.size);
-        REQUIRE(ma2.capacity() == 1);
         REQUIRE(ma2.size() == 1);
 
         ma2.deallocate_block();
@@ -65,7 +58,6 @@ TEST_CASE("memory_arena", "[memory_arena]")
     SECTION("swap")
     {
         memory_arena<growing_block_allocator<>> ma1(1024);
-        REQUIRE(ma1.capacity() == 0);
         REQUIRE(ma1.size() == 0);
 
         memory_block mb = ma1.allocate_block();
@@ -73,21 +65,17 @@ TEST_CASE("memory_arena", "[memory_arena]")
         REQUIRE(mb.size == 1024 - 16);
         REQUIRE(ma1.current_block().memory == mb.memory);
         REQUIRE(ma1.current_block().size == mb.size);
-        REQUIRE(ma1.capacity() == 1);
         REQUIRE(ma1.size() == 1);
 
         memory_arena<growing_block_allocator<>> ma2(0);
-        REQUIRE(ma2.capacity() == 0);
         REQUIRE(ma2.size() == 0);
 
         swap(ma1, ma2);
 
         REQUIRE(ma2.current_block().memory == mb.memory);
         REQUIRE(ma2.current_block().size == mb.size);
-        REQUIRE(ma2.capacity() == 1);
         REQUIRE(ma2.size() == 1);
 
-        REQUIRE(ma1.capacity() == 0);
         REQUIRE(ma1.size() == 0);
 
         ma2.deallocate_block();
@@ -98,7 +86,6 @@ TEST_CASE("memory_arena", "[memory_arena]")
         constexpr std::size_t block_size = 1024;
 
         memory_arena<growing_block_allocator<>> ma(block_size);
-        REQUIRE(ma.capacity() == 0);
         REQUIRE(ma.size() == 0);
 
         memory_block mb1 = ma.allocate_block();
@@ -106,7 +93,6 @@ TEST_CASE("memory_arena", "[memory_arena]")
         REQUIRE(mb1.size == block_size - 16);
         REQUIRE(ma.current_block().memory == mb1.memory);
         REQUIRE(ma.current_block().size == mb1.size);
-        REQUIRE(ma.capacity() == 1);
         REQUIRE(ma.size() == 1);
         REQUIRE(ma.next_block_size() == (block_size * 2) - 16);
 
@@ -115,7 +101,6 @@ TEST_CASE("memory_arena", "[memory_arena]")
         REQUIRE(mb2.size == (block_size * 2) - 16);
         REQUIRE(ma.current_block().memory == mb2.memory);
         REQUIRE(ma.current_block().size == mb2.size);
-        REQUIRE(ma.capacity() == 2);
         REQUIRE(ma.size() == 2);
         REQUIRE(ma.next_block_size() == (block_size * 4) - 16);
 
@@ -123,12 +108,10 @@ TEST_CASE("memory_arena", "[memory_arena]")
         ma.deallocate_block();
         REQUIRE(ma.current_block().memory == mb1.memory);
         REQUIRE(ma.current_block().size == mb1.size);
-        REQUIRE(ma.capacity() == 1);
         REQUIRE(ma.size() == 1);
         REQUIRE(ma.next_block_size() == (block_size * 4) - 16);
 
         ma.deallocate_block();
-        REQUIRE(ma.capacity() == 0);
         REQUIRE(ma.size() == 0);
         REQUIRE(ma.next_block_size() == (block_size * 4) - 16);
     }
@@ -138,7 +121,6 @@ TEST_CASE("memory_arena", "[memory_arena]")
         constexpr std::size_t block_size = 1024;
 
         memory_arena<growing_block_allocator<>> ma(block_size);
-        REQUIRE(ma.capacity() == 0);
         REQUIRE(ma.size() == 0);
 
         memory_block mb = ma.allocate_block();
@@ -146,7 +128,6 @@ TEST_CASE("memory_arena", "[memory_arena]")
         REQUIRE(mb.size == block_size - 16);
         REQUIRE(ma.current_block().memory == mb.memory);
         REQUIRE(ma.current_block().size == mb.size);
-        REQUIRE(ma.capacity() == 1);
         REQUIRE(ma.size() == 1);
         REQUIRE(ma.next_block_size() == (block_size * 2) - 16);
 
@@ -155,11 +136,23 @@ TEST_CASE("memory_arena", "[memory_arena]")
         REQUIRE_FALSE(ma.owns(static_cast<std::byte*>(mb.memory) - 17));
 
         ma.deallocate_block();
-        REQUIRE(ma.capacity() == 0);
         REQUIRE(ma.size() == 0);
         REQUIRE(ma.next_block_size() == (block_size * 2) - 16);
         REQUIRE_FALSE(ma.owns(static_cast<std::byte*>(mb.memory) + 1));
         REQUIRE_FALSE(ma.owns(nullptr));
         REQUIRE_FALSE(ma.owns(static_cast<std::byte*>(mb.memory) - 17));
+    }
+
+    SECTION("constexpr")
+    {
+        constexpr memory_arena<growing_block_allocator<lowlevel_allocator<malloc_allocator>, 2, 1>> ma(1024);
+
+        REQUIRE(ma.size() == 0);
+        REQUIRE(ma.next_block_size() == 1024 - 16);
+        REQUIRE_FALSE(ma.owns(nullptr));
+
+        constexpr memory_block mb = ma.current_block();
+        REQUIRE(mb.memory == nullptr);
+        REQUIRE(mb.size == 0);
     }
 }
